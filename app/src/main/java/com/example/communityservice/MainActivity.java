1f6caf1ed57fe7654a875fdb9fc2c197c9ab25c9
Infinity323 <paul_in_plano@hotmail.com>
2021-01-30 18:29:23 -0600

    attempting classes

--- app/src/main/java/com/example/communityservice/MainActivity.java
+++ app/src/main/java/com/example/communityservice/MainActivity.java
@@ -31,15 +31,21 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }); */
+    }
+
+    /** Called when user taps the + button */
+    public void addOrg(View view) {
+        // Do something
     }
 
     /** Called when the activity is about to become visible. */
